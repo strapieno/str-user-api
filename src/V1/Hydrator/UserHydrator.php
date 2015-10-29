1e1bc96e7d3a14215e4184d814f0c8975002782c
visa4 <antonino.visalli@gmail.com>
2015-10-29 16:20:36 +0100

    Fix import

--- src/V1/Hydrator/UserHydrator.php
+++ src/V1/Hydrator/UserHydrator.php
@@ -4,6 +4,8 @@ namespace Strapieno\User\Api\V1\Hydrator;
 
 use Matryoshka\Model\Hydrator\Strategy\DateTimeStrategy;
 use Strapieno\ModelUtils\Hydrator\DateHystoryHydrator;
+use Zend\Stdlib\Hydrator\Filter\FilterComposite;
+use Zend\Stdlib\Hydrator\Filter\MethodMatchFilter;
 
 /**
  * Class UserHydrator
