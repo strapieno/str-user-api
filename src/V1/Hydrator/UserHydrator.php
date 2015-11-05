6df2d07b351f644d953853d78f2e0e7e2f7cb05e
visa4 <antonino.visalli@gmail.com>
2015-11-05 12:56:52 +0100

    Add filter roleId to user hydrator

--- src/V1/Hydrator/UserHydrator.php
+++ src/V1/Hydrator/UserHydrator.php
@@ -26,5 +26,11 @@ class UserHydrator extends DateHystoryHydrator
             new MethodMatchFilter('getIdentity', true),
             FilterComposite::CONDITION_AND
         );
+
+        $this->filterComposite->addFilter(
+            'roleId',
+            new MethodMatchFilter('getRoleId', true),
+            FilterComposite::CONDITION_AND
+        );
     }
 }
